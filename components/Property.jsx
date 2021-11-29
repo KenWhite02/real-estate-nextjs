d918001a19d592bba69c32425e70c608f7843d57
KenWhite02 <whiteken2002@gmail.com>
2021-11-29 13:46:27 +0300

    Added property info

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,6 +6,8 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
+import defaultImage from '../assets/house.jpg';
+
 const Property = ({
   property: {
     coverPhoto,
@@ -22,7 +24,57 @@ const Property = ({
 }) => {
   return (
     <Link href={`/property/${externalID}`} passHref>
-      {title}
+      <Flex
+        flexWrap={'wrap'}
+        w={'420px'}
+        p={'5'}
+        paddingTop={'0'}
+        margin={'auto'}
+        cursor={'pointer'}
+      >
+        <Box>
+          <Image
+            src={coverPhoto ? coverPhoto.url : defaultImage}
+            width={400}
+            height={260}
+            alt={title}
+          />
+        </Box>
+
+        <Box w={'full'}>
+          <Flex
+            paddingTop={'2'}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+          >
+            <Flex alignItems={'center'}>
+              <Box paddingRight={'3'} color={'green.300'}>
+                {isVerified && <GoVerified />}
+              </Box>
+              <Text fontWeight={'bold'} fontSize={'lg'}>
+                AED {millify(price)}
+                {rentFrequency && `/${rentFrequency}`}
+              </Text>
+            </Flex>
+            <Box>
+              <Avatar size={'sm'} src={agency?.logo?.url} />
+            </Box>
+          </Flex>
+          <Flex
+            alignItems={'center'}
+            justifyContent={'space-between'}
+            padding={'1'}
+            w={'250px'}
+            color={'blue.400'}
+          >
+            {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{' '}
+            <BsGridFill />
+          </Flex>
+          <Text fontSize={'lg'}>
+            {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+          </Text>
+        </Box>
+      </Flex>
     </Link>
   );
 };
